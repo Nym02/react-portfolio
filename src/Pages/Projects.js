d4fa841927d84b6938b400144b21cc850cf433a1
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2021-07-26 03:07:16 +0600

    working on the portfolio

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,94 +1,61 @@
-// import ProjectComponents from "../components/Projects/ProjectComponents/ProjectComponents";
+import futbol from "../assets/futbol.png";
+import hungry from "../assets/hungry_monster.png";
+import kontest from "../assets/kontest.png";
+import outdoors from "../assets/outdoors.png";
+import trvl from "../assets/trvl.png";
+import ButtonBlue from "../components/Button/ButtonBlue";
+import ProjectComponents from "../components/Projects/ProjectComponents/ProjectComponents";
 
 const Projects = () => {
   return (
     <>
-      {/* <div className="flex justify-center items-center mt-12 text=2xl font-open font-light text-center p-2">
+      <div className="flex justify-center items-center mt-12 text=2xl font-open font-light text-center p-2">
         All web related landing page and projects i have done and currenly working on is given below,
       </div>
-      <div className="flex flex-col justify-center items-center lg:mt-24 mt-12">
-        <div className="flex flex-col lg:w-5/6 w-full justify-center items-center lg:px-0 px-2 space-y-9">
+      <div className="flex flex-col justify-center items-center w-full lg:mt-24 mt-12">
+        <div className="flex flex-col xxl:w-5/6 w-full justify-center items-center lg:px-0 px-2 space-y-9">
           <ProjectComponents
-            src="img/swiftex.jpg"
-            title="SwiftEx"
-            live="https://swiftex.app/"
-            git="https://github.com/fuzzy95/swiftex.git"
-            details="A complete delivery system for a upcoming delivery system in the market. This project is still under work and soon it will be completed."
+            src={futbol}
+            title="futbol"
+            live="https://vigorous-kilby-cddeb1.netlify.app/"
+            git="https://github.com/Nym02/futbol-react"
+            details="A simple website for viewing premier league team details"
           />
           <ProjectComponents
-            src="img/todo-list.jpg"
-            title="Todo list"
-            live="https://todo-list-drab.vercel.app"
-            git="https://github.com/fuzzy95/Todo-list.git"
-            details="A simple todo list app using React and React-Hook.
-                It can filter completed and uncompleted tasks and save tasks in the local storage."
-          />
-          <ProjectComponents
-            src="img/food-recipe1.jpg"
-            title="Food recipe"
-            live="https://food-recipe-searcher.vercel.app"
-            git="https://github.com/fuzzy95/Food-recipe-searcher.git"
-            details="Food Recipe searching web app using Edamam food recipe api in react."
-          />
-          <ProjectComponents
-            src="img/kitchen.jpg"
-            title="Kitchen"
-            live="https://advance-css-using-sass.vercel.app"
-            git="https://github.com/fuzzy95/Advance-Css-Using-Sass.git"
-            details="Restaurant website landing page design using advanced HTML, CSS using SASS."
-          />
-          <ProjectComponents
-            src="img/leica-m6.jpg"
-            title="Leica-m6"
-            live="https://leica-m6.vercel.app"
-            git="https://github.com/fuzzy95/Leica-m6.git"
-            details="A simple camera websites landing page using HTMl, CSS using SASS."
-          />
-          <ProjectComponents
-            src="img/travel.jpg"
+            src={trvl}
             title="Travel"
             live="https://travel-website-kappa.vercel.app"
-            git="https://github.com/fuzzy95/Travel-Website.git"
+            git="https://github.com/Nym02/travel-spa-react"
             details="A travel website landing page design using HTML, CSS in react."
           />
           <ProjectComponents
-            src="img/github-jobs.jpg"
-            title="Github Jobs"
-            live="https://github-job-listing-app.vercel.app"
-            git="https://github.com/fuzzy95/github-job-listing-app.git"
-            details="Github job listing web application using Github jobs api and tailwind css in react."
+            src={hungry}
+            title="Hungry Monster"
+            live="https://nym02.github.io/hungry-monster/"
+            git="https://github.com/Nym02/hungry-monster"
+            details="A simple food recipe search app using Vanilla JS."
           />
           <ProjectComponents
-            src="img/delivery.jpg"
-            title="PeonBD (clone)"
-            live="https://rapidex.vercel.app"
-            git="https://github.com/fuzzy95/delivery.git"
-            details="Clone of a delivery website known as PeonBD. Experimental clone to understand the scalability of design skill using react."
+            src={kontest}
+            title="Kontest"
+            live="https://nym02.github.io/kontest-programming/"
+            git="https://github.com/Nym02/kontest-programming"
+            details="A simple website where you can see all the programming contest is happening or will happen in the future."
           />
           <ProjectComponents
-            src="img/job-listing.jpg"
-            title="Job Listing App"
-            live="https://job-listing-app-dun.vercel.app"
-            git="https://github.com/fuzzy95/job-listing-app.git"
-            details="A job listing app in react using local JSON data and tailwind css that can filter through categories."
-          />
-          <ProjectComponents
-            src="img/natural-disaster.jpg"
-            title="Wildfire Tracker"
-            live="https://natural-disaster-tracker.vercel.app"
-            git="https://github.com/fuzzy95/natural-disaster-tracker.git"
-            details="Wildfire tracker app that can also be used as a natural disaster tracker app using NASA open api and google map api in react."
-          />
-          <ProjectComponents
-            src="img/mecha-watch.jpg"
-            title="Mecha-Watch"
-            live="https://mecha-watch.vercel.app"
-            git="https://github.com/fuzzy95/Mecha-watch.git"
-            details="A watch website using HTML, CSS and JavaScript and a trending style known as Neumorphism."
+            src={outdoors}
+            title="Holiday Home"
+            live="https://nym02.github.io/HolidayHomes/"
+            git="https://github.com/Nym02/HolidayHomes"
+            details="Travel Agency landing page using HTML5, CSS3 and SASS."
           />
         </div>
-      </div> */}
-      <h1>Coming Soon..</h1>
+        <hr />
+        <div className="mt-12">
+          <ButtonBlue to="/projects" text="View more" />
+        </div>
+      </div>
+      {/* <h1>Coming Soon..</h1> */}
     </>
   );
 };
